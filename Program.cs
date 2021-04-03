700d7c18938101cdb8cd34012cc545888d6e1c31
Thai Le <idiotcm910@gmail.com>
2021-04-03 11:08:12 +0700

    add Program.cs to source

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Game;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display_Sodoku Run = new Display_Sodoku();
+        }
+    }
+}
